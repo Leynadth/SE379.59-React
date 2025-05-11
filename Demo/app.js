c709230e4a8edf5ac2377a4e2750015353d99031
Leynadth <ljsosaortiz@email.neit.edu>
2025-05-10 22:02:59 -0400

    week1new
    week1new

--- Demo/app.js
+++ Demo/app.js
@@ -1,9 +1,29 @@
-const button = document.querySelector('button');
+const todos = ['Learn React', 'Start Coding', 'build a Project'];
+const todoContainer = document.querySelector('.todo-container');
+
+todos.forEach(todoText => {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  const cardContent = document.createElement('div');
+  cardContent.className = 'card-content';
+
+  const h2 = document.createElement('h2');
+  h2.textContent = todoText;
+
+  const button = document.createElement('button');
+  button.className = 'btn';
+  button.textContent = 'Done';
+  button.addEventListener('click', showModalHandler);
+
+  cardContent.appendChild(h2);
+  cardContent.appendChild(button);
+  card.appendChild(cardContent);
+  todoContainer.appendChild(card);
+});
 
 let modal;
 let backdrop;
-
-button.addEventListener('click', showModalHandler);
 
 function showModalHandler() {
   if (modal) {
@@ -14,7 +34,7 @@ function showModalHandler() {
   modal.className = 'modal';
 
   const modalText = document.createElement('p');
-  modalText.textContent = 'Did you complete this task?';
+  modalText.textContent = 'Did you complete the task';
 
   const modalCancelAction = document.createElement('button');
   modalCancelAction.textContent = 'Cancel';
@@ -22,7 +42,7 @@ function showModalHandler() {
   modalCancelAction.addEventListener('click', closeModalHandler);
 
   const modalConfirmAction = document.createElement('button');
-  modalConfirmAction.textContent = 'Confirm';
+  modalConfirmAction.textContent = 'Yes';
   modalConfirmAction.className = 'btn';
   modalConfirmAction.addEventListener('click', closeModalHandler);
 
@@ -34,7 +54,6 @@ function showModalHandler() {
 
   backdrop = document.createElement('div');
   backdrop.className = 'backdrop';
-
   backdrop.addEventListener('click', closeModalHandler);
 
   document.body.append(backdrop);
@@ -43,7 +62,6 @@ function showModalHandler() {
 function closeModalHandler() {
   modal.remove();
   modal = null;
-
   backdrop.remove();
   backdrop = null;
 }
